3eb70938f197ae7b8c8534011e8d2fd868e69bae
Jakub Jirous <info@jakubjirous.dev>
2024-09-15 12:05:33 -0700

    feat(testimonial-card): update semantic html and aria

--- src/components/testimonial-card.tsx
+++ src/components/testimonial-card.tsx
@@ -19,9 +19,13 @@ export default function TestimonialCard({
 	children?: ReactNode;
 }): ReactNode {
 	return (
-		<div className="w-[21.25rem] flex flex-col gap-4 bg-white p-6 rounded-lg shadow-testimonial-card">
+		<blockquote
+			role="blockquote"
+			aria-labelledby="testimonial-title"
+			className="w-[21.25rem] flex flex-col gap-4 bg-white p-6 rounded-lg shadow-testimonial-card"
+		>
 			<div className="grid grid-cols-[3rem,1fr] items-center gap-4 self-stretch">
-				<div className="w-12 h-12 rounded-full overflow-clip">
+				<figure className="w-12 h-12 rounded-full overflow-clip">
 					<Image
 						alt={alt}
 						className="w-full h-full object-cover"
@@ -30,9 +34,12 @@ export default function TestimonialCard({
 						src={src}
 						width={width}
 					/>
-				</div>
+				</figure>
 				<div className="flex flex-col gap-px flex-grow w-full truncate">
-					<h1 className="font-semibold text-lg text-justify text-neutral-900 truncate">
+					<h1
+						id="testimonial-title"
+						className="font-semibold text-lg text-justify text-neutral-900 truncate"
+					>
 						{title}
 					</h1>
 					<h2 className="font-normal text-sm text-neutral-600 truncate">
@@ -43,6 +50,6 @@ export default function TestimonialCard({
 			<p className="font-normal text-base text-neutral-600 line-clamp-5">
 				{children}
 			</p>
-		</div>
+		</blockquote>
 	);
 }
