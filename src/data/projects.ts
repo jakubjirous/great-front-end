dc93c55c463263b2eb7cb50d18f0ce3a91d6aa70
Jakub Jirous <info@jakubjirous.dev>
2024-09-15 12:07:15 -0700

    feat(projects): add blog card to main dashboard

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,16 +1,30 @@
 import { ProjectType } from "@/components/project-card";
+import blogCard from "../../public/assets/blog-card.webp";
 import testimonialCard from "../../public/assets/testimonial-card.webp";
 
 export const projects: ProjectType[] = [
 	{
 		title: "Testimonial Card",
-		description: "A simple user testimonial card with key testimonial elements",
+		description:
+			"A simple user testimonial card with key testimonial elements.",
 		href: "/testimonial-card",
 		thumbnail: {
 			src: testimonialCard,
 			alt: "Testimonial Card",
 			width: 2061,
 			height: 1161,
+		},
+	},
+	{
+		title: "Blog Card",
+		description:
+			"A blog card featuring editorial content, including an article cover image, a content category tag, a title, a brief description, and a call-to-action (CTA) link.",
+		href: "/blog-card",
+		thumbnail: {
+			src: blogCard,
+			alt: "Blog Card",
+			width: 2250,
+			height: 1674,
 		},
 	},
 ];
