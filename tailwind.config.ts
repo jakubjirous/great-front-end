6cd5908c82bc9871363604b2eaa51e322599639d
Jakub Jirous <info@jakubjirous.dev>
2024-09-14 18:30:08 -0700

    core: add tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,48 @@ const config: Config = {
 		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
-	theme: {},
+	theme: {
+		screens: {
+			mobile: "375px",
+			tablet: "768px",
+			desktop: "1440px",
+		},
+		container: {
+			center: true,
+			screens: {
+				mobile: "100%",
+				tablet: "100%",
+				desktop: "1280px",
+			},
+			padding: {
+				DEFAULT: "1rem", // 16px
+				mobile: "2rem", // 32px
+				tablet: "2rem", // 32px
+				desktop: "1rem", // 16px
+			},
+		},
+		extend: {
+			colors: {
+				iron: {
+					50: "#f9fafa",
+					100: "#f7f7f8",
+					200: "#eef0f1",
+					300: "#e3e5e8",
+					400: "#dadde2",
+					500: "#d2d6db",
+					600: "#a2aab4",
+					700: "#717e8e",
+					800: "#4d5660",
+					900: "#272b30",
+					950: "#121417",
+				},
+			},
+			boxShadow: {
+				card: "0px 1px 3px 0px rgba(0, 0, 0, 0.10), 0px 1px 2px -1px rgba(0, 0, 0, 0.10)",
+			},
+		},
+	},
 	plugins: [],
 };
+
 export default config;
