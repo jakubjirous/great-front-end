5cb26d230c826005f112bfc6cd2f95a12c333a16
Jakub Jirous <info@jakubjirous.dev>
2024-09-18 14:39:17 -0700

    refactor: update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,29 +1,31 @@
 import { config } from "@/utils/config";
-import { footerLink } from "@/utils/shared";
 import Link from "next/link";
+
+const linkStyles =
+	"font-semibold text-neutral-700 duration-500 transition-all border-solid border-b border-b-transparent hover:border-neutral-700";
 
 export default function Footer() {
 	return (
-		<footer className="fixed bottom-0 text-xs flex justify-center w-full p-4 text-neutral-500 bg-transparent backdrop-blur-md">
-			A challenge by{" "}
+		<footer className="fixed bottom-0 text-xs flex justify-center w-full p-4 text-neutral-500 bg-transparent backdrop-blur-md gap-0.5">
+			<span>A challenge by</span>
 			<Link
 				target="_blank"
 				rel="no-follow"
 				href={config.hrefGEF}
-				className={footerLink}
+				className={linkStyles}
 			>
 				GreatFrontEnd Projects
 			</Link>
-			. Built by{" "}
+			<span className="px-1">|</span>
+			<span>Built by</span>
 			<Link
 				target="_blank"
 				rel="no-follow"
 				href={config.hrefJJ}
-				className={footerLink}
+				className={linkStyles}
 			>
 				Jakub Jirous
 			</Link>
-			.
 		</footer>
 	);
 }
