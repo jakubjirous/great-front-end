9aa2b1544740fe715a12c2958671301dbb086910
Jakub Jirous <info@jakubjirous.dev>
2024-09-18 14:38:53 -0700

    core: install svgr

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -48,8 +48,7 @@ const config: Config = {
 			boxShadow: {
 				"testimonial-card":
 					"0px 1px 3px 0px rgba(0, 0, 0, 0.10), 0px 1px 2px -1px rgba(0, 0, 0, 0.10)",
-				"blog-card":
-					"0px 1px 3px 0px rgba(0, 0, 0, 0.10), 0px 1px 2px 0px rgba(0, 0, 0, 0.06);",
+				card: "0px 1px 3px 0px rgba(0, 0, 0, 0.10), 0px 1px 2px 0px rgba(0, 0, 0, 0.06);",
 			},
 		},
 	},
